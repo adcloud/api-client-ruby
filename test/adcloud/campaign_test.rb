f22a88823937fde08d626fea2cf8d74997a20d56
Maximilian Schulz <m.schulz@kulturfluss.de>
2012-08-14 18:30:11 +0200

    Handle errors during validation and add validation tests.

--- test/adcloud/campaign_test.rb
+++ test/adcloud/campaign_test.rb
@@ -4,6 +4,7 @@ require 'test_helper'
 describe Adcloud::Campaign do
   subject { Adcloud::Campaign }
 
+  let(:campaign) { subject.new }
   let(:connection) { stub() }
 
   before do
@@ -71,11 +72,41 @@ describe Adcloud::Campaign do
   end
 
   describe "validate" do
+    it 'should validate against the api' do
+      connection.expects(:get).with('campaigns/validate', campaign.attributes).returns({'_meta' => { 'status' => 226, 'details' => {}}})
+      campaign.validate
+    end
 
-    it "should validate parameters"
+    it 'should populate the errors hash' do
+      response_data = {'_meta' => { 'status' => 226, 'details' => { 'company_id' => ['must be present'] } } }
+      connection.expects(:get).with('campaigns/validate', campaign.attributes).returns(response_data)
+      campaign.validate
+      campaign.errors['company_id'].must_equal ['must be present']
+    end
 
-    it "should check if campaign is valid"
+    it 'raises an exception when the response is not well formatted' do
+      connection.expects(:get).with('campaigns/validate', campaign.attributes)
+      -> { campaign.validate }.must_raise(Adcloud::AdcloudUnknownAPIError::InvalidApiResponse)
+    end
+  end
 
+  describe '#valid?' do
+    it 'validates the model' do
+      campaign.expects(:validate)
+      campaign.valid?
+    end
+
+    it 'returns true when object is valid' do
+      campaign.stubs(:validate)
+      campaign.errors = {}
+      campaign.valid?.must_equal true
+    end
+
+    it 'returns false when the object is invalid' do
+      campaign.stubs(:validate)
+      campaign.errors = { company_id: ['must be present'] }
+      campaign.valid?.must_equal false
+    end
   end
 
   describe "create" do
@@ -83,7 +114,7 @@ describe Adcloud::Campaign do
     it "should create a campaign"
 
     it "should not create a campaign"
-     
+
   end
 
 end

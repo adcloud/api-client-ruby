2cc51d53d44c85d521b598e02510ce428c6de8b4
Maximilian Schulz <m.schulz@kulturfluss.de>
2012-08-27 13:33:35 +0200

    Remove explicit api_endpoint definitions.

--- lib/adcloud/customer.rb
+++ lib/adcloud/customer.rb
@@ -1,13 +1,9 @@
 module Adcloud
-
   class Customer < Adcloud::Entity
-    self.api_endpoint = 'customers'
-
     attribute :_meta, Hash
     attribute :id, Integer
     attribute :name, Boolean
     attribute :modified, DateTime
     attribute :created, DateTime
   end
-
 end

05d3662efc98baba10d750c237942c727b66a2a0
Maximilian Schulz <m.schulz@kulturfluss.de>
2012-09-10 07:53:30 +0200

    Change topic discount format to behave like the name hash.

--- test/adcloud/topic_test.rb
+++ test/adcloud/topic_test.rb
@@ -12,4 +12,13 @@ describe Adcloud::Topic do
       topic.names.must_equal({ 'es_es' => 'Entretenimiento' })
     end
   end
+
+  describe '#discounts' do
+    let(:raw_data) { [{ 'country_id' => 11, 'discount' => '0.0', 'reach' => '4.0', 'min_price_cpc' => 0, 'price_cpc' => 0, 'max_price_cpc' => 0, 'min_price_cpm' => 0, 'price_cpm' => 0, 'max_price_cpm' => 0, 'country_code' => 'es' }] }
+    it 'transforms the data into a hash with locale as key' do
+      topic = Adcloud::Topic.new
+      topic.discounts = raw_data
+      topic.discounts['es'].must_be_instance_of Adcloud::TopicDiscount
+    end
+  end
 end

4df35fe2c1a9d0ef237f10548bcc956d0f004430
Maximilian Schulz <m.schulz@kulturfluss.de>
2012-11-20 18:12:56 +0100

    Log failed reponses

--- lib/adcloud/exception_raiser.rb
+++ lib/adcloud/exception_raiser.rb
@@ -3,6 +3,8 @@ module Adcloud
   class ExceptionRaiser
 
     def initialize(response)
+      Adcloud.logger.warn { "API Exception #{response.inspect}" }
+
       case response.status
       when 400
         raise Adcloud::BadRequestError.new(response)
